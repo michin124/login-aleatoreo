dc1cc8bf2a3f6015fb0fbfebf54e9dad96436546
Miguel <miguelvargasp@usantotomas.edu.co>
2026-04-23 18:46:31 -0500

    prueba

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './Apis/ApiTab1';
+import Tab1 from './pages/Tab1';
 
 
 
@@ -30,6 +30,16 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
@@ -45,6 +55,7 @@ const App: React.FC = () => (
           <Route exact path="/tab1">
             <Tab1 />
           </Route>
+
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,6 +2,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 
 import {
   IonApp, 
@@ -9,16 +12,40 @@ import {
   IonList,
   IonItem,
   IonLabel,
-  IonButton
+  IonRow,
+  IonButton,
+  IonLoading 
 } from '@ionic/react';
 
-const Tab1 = (response:any) => {
+const Tab1: React.FC  = () => {
+  const [menssage, setMenssage] = useState('');
+  const [error, setError] = useState(false);
+  const [nresultNumber, setResultNumber] = useState('');
+  const [loading, setLoading] = useState(false);
+  const calculator = (number:any) =>{
+    setLoading(true);
+    let url=`https://newton.vercel.app/api/v2/factor/${number}%5E2-1`
+    
+      axios.get(url)
+      .then(res => {
+
+        const response = res.data;
+        setMenssage(response.operation)
+        setResultNumber(response.result)
+        setError(false);
+      })
+      .catch(err => {
+        
+        setError(true);
+      }).finally(() => { 
+        setLoading(false);
+      });
+      
+
+  }
+
   const [data, setData] = useState({});
-  useEffect(() => {
-    response(data)
-  });
-
-
+ {console.log(error)}
 
   return (
     <IonPage>
@@ -34,9 +61,49 @@ const Tab1 = (response:any) => {
           </IonToolbar>
         </IonHeader>
         <IonItem>
-          <IonInput label="Ingresa un numero" onIonChange={({ detail }) => setData(detail.value!)}></IonInput>
+          <IonInput label="Ingresa un numero" onIonInput={({ detail }) => setData(detail.value!)}></IonInput>
         </IonItem>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={({ detail }) => calculator(data)} >Calcular</IonButton>
+        {loading == false&&
+          <IonItem>cargando</IonItem>
+    
+
+        }
+        
+        {loading == false&&
+         
+          <IonRow>
+            {error==false&&
+              <IonList>
+              <IonItem>
+                <IonLabel>
+                  El tipo de operacion es {menssage}
+                </IonLabel>
+              </IonItem>
+
+              <IonItem>
+                <IonLabel class="ion-text-nowrap">
+                  El resultado es: {nresultNumber}
+                </IonLabel>
+              </IonItem>
+                
+              </IonList>
+            }
+            {error==true&&
+             
+                
+              <IonRow>Error en la operacion{error}</IonRow>
+              
+            }
+          
+
+          </IonRow>
+        }
+        
+        
+        <IonRow>
+
+        </IonRow>
       </IonContent>
     </IonPage>
   );
